5c930d77759fbcacc91b611f54939ef73397ed5c
Tran Hoa Binh <binh.tran.hoa@sofiamedix.vn>
2016-12-09 17:42:07 +0700

    [Update] Revert code

--- app/src/main/java/com/badr/infodota/base/menu/fragment/MenuFragment.java
+++ app/src/main/java/com/badr/infodota/base/menu/fragment/MenuFragment.java
@@ -1,6 +1,5 @@
 package com.badr.infodota.base.menu.fragment;
 
-import android.app.Activity;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -12,7 +11,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import com.badr.infodota.R;
 import com.badr.infodota.base.activity.ListHolderActivity;
@@ -21,6 +19,8 @@ import com.badr.infodota.base.menu.adapter.DHMenuAdapter;
 import java.lang.ref.WeakReference;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * User: Histler
@@ -38,6 +38,9 @@ public class MenuFragment extends Fragment implements DHMenuAdapter.OnItemClickL
     @BindView(R.id.recyclerMenu)
     RecyclerView mRecyclerMenu;
 
+    protected View mView;
+    protected Unbinder mUnbinder;
+
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.clear();
@@ -49,35 +52,32 @@ public class MenuFragment extends Fragment implements DHMenuAdapter.OnItemClickL
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (mView == null) {
+            mView = inflater.inflate(R.layout.fragment_menu, container, false);
+            mUnbinder = ButterKnife.bind(this, mView);
+        }
         DHMenuAdapter adapterMenu = new DHMenuAdapter(getContext());
         mRecyclerMenu.setAdapter(adapterMenu);
         adapterMenu.setOnItemClickListener(this);
         mRecyclerMenu.setHasFixedSize(false);
         mRecyclerMenu.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
         mRecyclerMenu.setNestedScrollingEnabled(false);
-        return inflater.inflate(R.layout.fragment_menu, container, false);
+        return mView;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setHasOptionsMenu(true);
-        setConfiguration();
+
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        setConfiguration();
     }
 
-    private void setConfiguration() {
-        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
-            ((LinearLayout) getView().findViewById(R.id.buttonsHolder)).setOrientation(LinearLayout.HORIZONTAL);
-        } else {
-            ((LinearLayout) getView().findViewById(R.id.buttonsHolder)).setOrientation(LinearLayout.VERTICAL);
-        }
-    }
+
 
     @Override
     public void onClick(View view, int position) {
